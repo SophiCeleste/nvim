7cd53b20fdfae1505d0edd56770e43d6a38038c0
Sophia Celeste <62121594+SophiCeleste@users.noreply.github.com>
2026-05-04 16:51:12 -0500

    Misc updates

--- init.lua
+++ init.lua
@@ -274,6 +274,16 @@ require('lazy').setup({
   },
 
   {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
+  {
     --- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -368,6 +378,64 @@ require('lazy').setup({
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter').setup {
+        ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'html', 'css' },
+        auto_install = false,
+        sync_install = false,
+        ignore_install = {},
+        highlight = { enable = true },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<M-space>',
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = { ['<leader>a'] = '@parameter.inner' },
+            swap_previous = { ['<leader>A'] = '@parameter.inner' },
+          },
+        },
+      }
+    end,
   },
 
   --- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
@@ -447,6 +515,7 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 --- Diagnostic keymaps
+vim.diagnostic.config({ virtual_text = true }) -- Enables inline diagnostic messages
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 --- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
@@ -461,6 +530,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
   group = highlight_group,
   pattern = '*',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'text' },
+  callback = function()
+    vim.opt_local.wrap = true
+    -- Optional: wrap at work poundaries rather than middle of words
+    vim.opt_local.linebreak = true
+  end,
 })
 
 --- [[ Configure Telescope ]]
@@ -543,79 +621,6 @@ vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by 
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
---- [[ Configure Treesitter ]]
---- See `:help nvim-treesitter`
---- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
-vim.defer_fn(function()
-  require('nvim-treesitter.configs').setup {
-    --- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'html', 'css' },
-
-    --- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
-    --- Install languages synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-    --- List of parsers to ignore installing
-    ignore_install = {},
-    --- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
-    modules = {},
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, --- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          --- You can use the capture groups defined in textobjects.scm
-          ['aa'] = '@parameter.outer',
-          ['ia'] = '@parameter.inner',
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, --- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
-    },
-  }
-end, 0)
 
 ---  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
@@ -735,12 +740,13 @@ mason_lspconfig.setup {
 }
 
 local function setup_one(server)
-  require('lspconfig')[server].setup({
+  vim.lsp.config(server, {
     capabilities = capabilities,
     on_attach = on_attach,
     settings = servers[server],
     filetypes = (servers[server] or {}).filetypes,
   })
+  vim.lsp.enable(server)
 end
 
 -- Prefer setup_handlers when available; otherwise fall back.
@@ -809,5 +815,20 @@ cmp.setup {
 --- [[ PROFILE SETUP ]]
 --- Sophia's profile
 require('sophia')
+
+--- Save folds and cursor posititon
+vim.opt.viewoptions = { "folds", "cursor" }
+
+local save_fold = vim.api.nvim_create_augroup("PersistentFolds", { clear = true })
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  group = save_fold,
+  pattern = "*.*",
+  command = "mkview",
+})
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = save_fold,
+  pattern = "*.*",
+  command = "silent! loadview",
+})
 --- The line beneath this is called `modeline`. See `:help modeline`
 --- vim: ts=2 sts=2 sw=2 et
--- lua/sophia/remap.lua
+++ lua/sophia/remap.lua
@@ -1,13 +1,41 @@
 local builtin = require('telescope.builtin')
+
+-- Leader
 vim.g.mapleader = ' '
+
+-- File / session
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'File [E]xplorer' })
 vim.keymap.set('n', '<leader>w', vim.cmd.w, { desc = '[W]rite' })
-vim.keymap.set('i', 'kj', '<esc>')
-vim.keymap.set('n', '<leader><leader>', vim.cmd.so, { desc = '[S]ource' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles in current dir' })
-vim.keymap.set('n', '<C-g>', builtin.git_files, { desc = 'Search current [G]it dir' })
 vim.keymap.set('n', '<leader>q', vim.cmd.q, { desc = '[Q]uit' })
 vim.keymap.set('n', '<leader>fq', ':q!<CR>', { desc = '[F]orce [Q]uit' })
+vim.keymap.set('n', '<leader><leader>', vim.cmd.so, { desc = '[S]ource current file' })
+
+-- Search (Telescope)
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<C-g>', builtin.git_files, { desc = 'Search [G]it files' })
+
+-- Window
 vim.keymap.set('n', 'gw', '<C-w>', { desc = '[G]et [W]indow' })
--- Replaces all occurences of the word under the cursor; requires confirmation
-vim.keymap.set('n', '<leader>/r', [[:%s/<C-r><C-w>//gc<Left><Left><Left>]], { desc = 'Find[/R]eplace' })
+
+-- Editing
+-- Replace all occurrences of word under cursor; prompts confirmation per match
+vim.keymap.set('n', '<leader>/r', [[:%s/<C-r><C-w>//gc<Left><Left><Left>]], { desc = '[/R]eplace word' })
+vim.keymap.set('n', '<A-k>', ':m-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set('n', '<A-j>', ':m-2<CR>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<leader>ff', 'zf', { desc = 'Create [F]old' })
+vim.keymap.set('n', '<leader>ft', 'za', { desc = '[F]old [T]oggle' })
+vim.keymap.set('n', '<leader>fk', '[z', { desc = 'Jump to [F]old start' })
+vim.keymap.set('n', '<leader>fj', ']z', { desc = 'Jump to [F]old end' })
+vim.keymap.set('n', '<leader>fJ', 'zj', { desc = 'Jump to next [F]old' })
+vim.keymap.set('n', '<leader>fK', 'zk', { desc = 'Jump to previous [F]old' })
+
+-- Visual mode
+vim.keymap.set('v', '<leader>ff', 'zf', { desc = 'Create [F]old' })
+
+-- Insert mode
+vim.keymap.set('i', 'kj', '<esc>') -- escape shortcut
+
+-- Symbols
+vim.keymap.set('i', '<C-s>a', 'μ', { desc = '[A]verage' })
+vim.keymap.set('i', '<C-s>sd', 'σ', { desc = '[S]tandard [D]eviation' })
+vim.keymap.set('i', '<C-s>fa', '∀', { desc = '[F]or [A]ll' })
